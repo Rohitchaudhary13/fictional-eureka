9a740ca4106bada42fefe3a5d0c9a9c62917fbbf
Bloodsplash <abhinavchaudhary1318@gmail.com>
2023-11-25 13:37:40 +0530

    three

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+import { App as Canvas } from './Canvas'
+import { Overlay } from './Overlay'
+function App() {
+
+  return (
+    <>
+     <Canvas />
+    <Overlay />
+    </>
+  )
+}
+
+export default App
